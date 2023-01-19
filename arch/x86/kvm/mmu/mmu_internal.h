e18ba42247940afa4fa445bfb8a9cb08d9b43453
David Matlack <dmatlack@google.com>
2023-01-19 09:35:58 -0800

    KVM: Allow range-based TLB invalidation from common code
    Make kvm_flush_remote_tlbs_range() visible in common code and create a
    default implementation that just invalidates the whole TLB.
    
    This paves the way for several future cleanups:
     - Introduction of range-based TLBI on ARM.
     - Eliminating kvm_arch_flush_remote_tlbs_memslot()
     - Moving the KVM/x86 TDP MMU to common code.
    
    No functional change intended.
    
    Signed-off-by: David Matlack <dmatlack@google.com>
    Message-Id: <20230119173559.2517103-7-dmatlack@google.com>

--- arch/x86/kvm/mmu/mmu_internal.h
+++ arch/x86/kvm/mmu/mmu_internal.h
@@ -164,7 +164,6 @@ void kvm_mmu_gfn_allow_lpage(const struct kvm_memory_slot *slot, gfn_t gfn);
 bool kvm_mmu_slot_gfn_write_protect(struct kvm *kvm,
 				    struct kvm_memory_slot *slot, u64 gfn,
 				    int min_level);
-void kvm_flush_remote_tlbs_range(struct kvm *kvm, gfn_t start_gfn, u64 pages);
 unsigned int pte_list_count(struct kvm_rmap_head *rmap_head);
 
 extern int nx_huge_pages;
