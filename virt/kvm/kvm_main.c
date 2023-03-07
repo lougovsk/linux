1fc529da42be5d1404a0aa6a5f6ad62c7103cac8
Ricardo Koller <ricarkol@google.com>
2023-03-07 03:45:50 +0000

    KVM: arm64: Export kvm_are_all_memslots_empty()
    Export kvm_are_all_memslots_empty(). This will be used by a future
    commit when checking before setting a capability.
    
    No functional change intended.
    
    Signed-off-by: Ricardo Koller <ricarkol@google.com>
    Reviewed-by: Shaoqin Huang <shahuang@redhat.com>
    Message-Id: <20230307034555.39733-8-ricarkol@google.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -4596,7 +4596,7 @@ int __attribute__((weak)) kvm_vm_ioctl_enable_cap(struct kvm *kvm,
 	return -EINVAL;
 }
 
-static bool kvm_are_all_memslots_empty(struct kvm *kvm)
+bool kvm_are_all_memslots_empty(struct kvm *kvm)
 {
 	int i;
 
