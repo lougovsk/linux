5d5651c6c2e8d63ebee4599051e81a2b04261345
David Matlack <dmatlack@google.com>
2023-06-21 17:49:54 +0000

    KVM: Allow range-based TLB invalidation from common code
    Make kvm_flush_remote_tlbs_range() visible in common code and create a
    default implementation that just invalidates the whole TLB.
    
    This paves the way for several future features/cleanups:
    
     - Introduction of range-based TLBI on ARM.
     - Eliminating kvm_arch_flush_remote_tlbs_memslot()
     - Moving the KVM/x86 TDP MMU to common code.
    
    No functional change intended.
    
    Signed-off-by: David Matlack <dmatlack@google.com>
    Signed-off-by: Raghavendra Rao Ananta <rananta@google.com>
    Message-Id: <20230621175002.2832640-4-rananta@google.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1804,6 +1804,9 @@ static inline int kvm_arch_flush_remote_tlbs(struct kvm *kvm)
 		return -ENOTSUPP;
 }
 
+#define __KVM_HAVE_ARCH_FLUSH_REMOTE_TLBS_RANGE
+int kvm_arch_flush_remote_tlbs_range(struct kvm *kvm, gfn_t start_gfn, u64 pages);
+
 #define kvm_arch_pmi_in_guest(vcpu) \
 	((vcpu) && (vcpu)->arch.handling_intr_from_guest)
 
