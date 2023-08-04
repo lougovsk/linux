c920e9e87476861502750d3ac55dbefde6d15f0f
Sean Christopherson <seanjc@google.com>
2023-08-03 17:42:26 -0700

    KVM: selftests: Add logic to detect if ioctl() failed because VM was killed
    Add yet another macro to the VM/vCPU ioctl() framework to detect when an
    ioctl() failed because KVM killed/bugged the VM, i.e. when there was
    nothing wrong with the ioctl() itself.  If KVM kills a VM, e.g. by way of
    a failed KVM_BUG_ON(), all subsequent VM and vCPU ioctl()s will fail with
    -EIO, which can be quite misleading and ultimately waste user/developer
    time.
    
    Use KVM_CHECK_EXTENSION on KVM_CAP_USER_MEMORY to detect if the VM is
    dead and/or bug, as KVM doesn't provide a dedicated ioctl().  Using a
    heuristic is obviously less than ideal, but practically speaking the logic
    is bulletproof barring a KVM change, and any such change would arguably
    break userspace, e.g. if KVM returns something other than -EIO.
    
    Without the detection, tearing down a bugged VM yields a cryptic failure
    when deleting memslots:
    
      ==== Test Assertion Failure ====
      lib/kvm_util.c:689: !ret
      pid=45131 tid=45131 errno=5 - Input/output error
         1	0x00000000004036c3: __vm_mem_region_delete at kvm_util.c:689
         2	0x00000000004042f0: kvm_vm_free at kvm_util.c:724 (discriminator 12)
         3	0x0000000000402929: race_sync_regs at sync_regs_test.c:193
         4	0x0000000000401cab: main at sync_regs_test.c:334 (discriminator 6)
         5	0x0000000000416f13: __libc_start_call_main at libc-start.o:?
         6	0x000000000041855f: __libc_start_main_impl at ??:?
         7	0x0000000000401d40: _start at ??:?
      KVM_SET_USER_MEMORY_REGION failed, rc: -1 errno: 5 (Input/output error)
    
    Which morphs into a more pointed error message with the detection:
    
      ==== Test Assertion Failure ====
      lib/kvm_util.c:689: false
      pid=80347 tid=80347 errno=5 - Input/output error
         1	0x00000000004039ab: __vm_mem_region_delete at kvm_util.c:689 (discriminator 5)
         2	0x0000000000404660: kvm_vm_free at kvm_util.c:724 (discriminator 12)
         3	0x0000000000402ac9: race_sync_regs at sync_regs_test.c:193
         4	0x0000000000401cb7: main at sync_regs_test.c:334 (discriminator 6)
         5	0x0000000000418263: __libc_start_call_main at libc-start.o:?
         6	0x00000000004198af: __libc_start_main_impl at ??:?
         7	0x0000000000401d90: _start at ??:?
      KVM killed/bugged the VM, check the kernel log for clues
    
    Suggested-by: Michal Luczaj <mhal@rbox.co>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20230804004226.1984505-5-seanjc@google.com>

--- tools/testing/selftests/kvm/include/kvm_util_base.h
+++ tools/testing/selftests/kvm/include/kvm_util_base.h
@@ -269,18 +269,44 @@ static __always_inline void static_assert_is_vm(struct kvm_vm *vm) { }
 	kvm_do_ioctl((vm)->fd, cmd, arg);			\
 })
 
+/*
+ * Assert that a VM or vCPU ioctl() succeeded, with extra magic to detect if
+ * the ioctl() failed because KVM killed/bugged the VM.  To detect a dead VM,
+ * probe KVM_CAP_USER_MEMORY, which (a) has been supported by KVM since before
+ * selftests existed and (b) should never outright fail, i.e. is supposed to
+ * return 0 or 1.  If KVM kills a VM, KVM returns -EIO for all ioctl()s for the
+ * VM and its vCPUs, including KVM_CHECK_EXTENSION.
+ */
+#define TEST_ASSERT_VM_VCPU_IOCTL(cond, name, ret, vm)					\
+do {											\
+	int __errno = errno;								\
+											\
+	static_assert_is_vm(vm);							\
+											\
+	if (cond)									\
+		break;									\
+											\
+	if (errno == EIO &&								\
+	    __vm_ioctl(vm, KVM_CHECK_EXTENSION, (void *)KVM_CAP_USER_MEMORY) < 0) {	\
+		TEST_ASSERT(errno == EIO, "KVM killed the VM, should return -EIO");	\
+		TEST_FAIL("KVM killed/bugged the VM, check the kernel log for clues");	\
+	}										\
+	errno = __errno;								\
+	TEST_ASSERT(cond, __KVM_IOCTL_ERROR(name, ret));				\
+} while (0)
+
 #define vm_ioctl(vm, cmd, arg)					\
 ({								\
 	int ret = __vm_ioctl(vm, cmd, arg);			\
 								\
-	TEST_ASSERT(!ret, __KVM_IOCTL_ERROR(#cmd, ret));	\
+	TEST_ASSERT_VM_VCPU_IOCTL(!ret, #cmd, ret, vm);		\
 })
 
 #define vm_fd_ioctl(vm, cmd, arg)				\
 ({								\
 	int fd = __vm_ioctl(vm, cmd, arg);			\
 								\
-	TEST_ASSERT(fd >= 0, __KVM_IOCTL_ERROR(#cmd, fd));	\
+	TEST_ASSERT_VM_VCPU_IOCTL(fd >= 0, #cmd, fd, vm);	\
 	fd;							\
 })
 
@@ -296,14 +322,14 @@ static __always_inline void static_assert_is_vcpu(struct kvm_vcpu *vcpu) { }
 ({								\
 	int ret = __vcpu_ioctl(vcpu, cmd, arg);			\
 								\
-	TEST_ASSERT(!ret, __KVM_IOCTL_ERROR(#cmd, ret));	\
+	TEST_ASSERT_VM_VCPU_IOCTL(!ret, #cmd, ret, (vcpu)->vm);	\
 })
 
 #define vcpu_fd_ioctl(vcpu, cmd, arg)				\
 ({								\
 	int fd = __vcpu_ioctl(vcpu, cmd, arg);			\
 								\
-	TEST_ASSERT(fd >= 0, __KVM_IOCTL_ERROR(#cmd, fd));	\
+	TEST_ASSERT_VM_VCPU_IOCTL(fd >= 0, #cmd, fd, (vcpu)->vm);\
 	fd;							\
 })
 
