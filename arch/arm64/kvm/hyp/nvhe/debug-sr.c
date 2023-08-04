91d775392ea39060b1fb96c768b0a178346e7d37
James Clark <james.clark@arm.com>
2023-08-04 11:13:12 +0100

    arm64: KVM: Support exclude_guest for Coresight trace in nVHE
    Currently trace will always be generated in nVHE as long as TRBE isn't
    being used. To allow filtering out guest trace, re-apply the filter
    rules before switching to the guest.
    
    The TRFCR restore function remains the same.
    
    Signed-off-by: James Clark <james.clark@arm.com>
    Message-Id: <20230804101317.460697-3-james.clark@arm.com>

--- arch/arm64/kvm/hyp/nvhe/debug-sr.c
+++ arch/arm64/kvm/hyp/nvhe/debug-sr.c
@@ -51,13 +51,17 @@ static void __debug_restore_spe(u64 pmscr_el1)
 	write_sysreg_s(pmscr_el1, SYS_PMSCR_EL1);
 }
 
-static void __debug_save_trace(u64 *trfcr_el1)
+/*
+ * Save TRFCR and disable trace completely if TRBE is being used. Return true
+ * if trace was disabled.
+ */
+static bool __debug_save_trace(u64 *trfcr_el1)
 {
 	*trfcr_el1 = 0;
 
 	/* Check if the TRBE is enabled */
 	if (!(read_sysreg_s(SYS_TRBLIMITR_EL1) & TRBLIMITR_EL1_E))
-		return;
+		return false;
 	/*
 	 * Prohibit trace generation while we are in guest.
 	 * Since access to TRFCR_EL1 is trapped, the guest can't
@@ -68,6 +72,8 @@ static void __debug_save_trace(u64 *trfcr_el1)
 	isb();
 	/* Drain the trace buffer to memory */
 	tsb_csync();
+
+	return true;
 }
 
 static void __debug_restore_trace(u64 trfcr_el1)
@@ -79,14 +85,55 @@ static void __debug_restore_trace(u64 trfcr_el1)
 	write_sysreg_s(trfcr_el1, SYS_TRFCR_EL1);
 }
 
+#if IS_ENABLED(CONFIG_PERF_EVENTS)
+static inline void __debug_save_trfcr(struct kvm_vcpu *vcpu)
+{
+	u64 trfcr;
+	struct kvm_etm_event etm_event = vcpu->arch.host_debug_state.etm_event;
+
+	/* No change if neither are excluded */
+	if (!etm_event.exclude_guest && !etm_event.exclude_host) {
+		/* Zeroing prevents restoring a stale value */
+		vcpu->arch.host_debug_state.trfcr_el1 = 0;
+		return;
+	}
+
+	trfcr = read_sysreg_s(SYS_TRFCR_EL1);
+	vcpu->arch.host_debug_state.trfcr_el1 = trfcr;
+
+	if (etm_event.exclude_guest) {
+		trfcr &= ~(TRFCR_ELx_ExTRE | TRFCR_ELx_E0TRE);
+	} else {
+		/*
+		 * If host was excluded then EL0 and ELx tracing bits will
+		 * already be cleared so they need to be set now for the guest.
+		 */
+		trfcr |= etm_event.exclude_kernel ? 0 : TRFCR_ELx_ExTRE;
+		trfcr |= etm_event.exclude_user ? 0 : TRFCR_ELx_E0TRE;
+	}
+	write_sysreg_s(trfcr, SYS_TRFCR_EL1);
+}
+#else
+static inline void __debug_save_trfcr(struct kvm_vcpu *vcpu) {}
+#endif
+
 void __debug_save_host_buffers_nvhe(struct kvm_vcpu *vcpu)
 {
+	bool trc_disabled = false;
+
 	/* Disable and flush SPE data generation */
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_SPE))
 		__debug_save_spe(&vcpu->arch.host_debug_state.pmscr_el1);
 	/* Disable and flush Self-Hosted Trace generation */
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRBE))
-		__debug_save_trace(&vcpu->arch.host_debug_state.trfcr_el1);
+		trc_disabled = __debug_save_trace(&vcpu->arch.host_debug_state.trfcr_el1);
+
+	/*
+	 * As long as trace wasn't completely disabled due to use of TRBE,
+	 * TRFCR can be saved and the exclude_guest rules applied.
+	 */
+	if (!trc_disabled && vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRFCR))
+		__debug_save_trfcr(vcpu);
 }
 
 void __debug_switch_to_guest(struct kvm_vcpu *vcpu)
@@ -98,7 +145,8 @@ void __debug_restore_host_buffers_nvhe(struct kvm_vcpu *vcpu)
 {
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_SPE))
 		__debug_restore_spe(vcpu->arch.host_debug_state.pmscr_el1);
-	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRBE))
+	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRBE) ||
+	    vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRFCR))
 		__debug_restore_trace(vcpu->arch.host_debug_state.trfcr_el1);
 }
 
