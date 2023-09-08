510b34352ccecd8be5472bc13bfcf0422a7241fe
Anish Moorthy <amoorthy@google.com>
2023-09-08 22:28:55 +0000

    KVM: Allow hva_pfn_fast() to resolve read faults
    hva_to_pfn_fast() currently just fails for faults where establishing
    writable mappings is forbidden, which is unnecessary. Instead, try
    getting the page without passing FOLL_WRITE. This allows the
    aforementioned faults to (potentially) be resolved without falling back
    to slow GUP.
    
    Suggested-by: James Houghton <jthoughton@google.com>
    Signed-off-by: Anish Moorthy <amoorthy@google.com>
    Message-Id: <20230908222905.1321305-9-amoorthy@google.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -2508,7 +2508,7 @@ static inline int check_user_page_hwpoison(unsigned long addr)
 }
 
 /*
- * The fast path to get the writable pfn which will be stored in @pfn,
+ * The fast path to get the pfn which will be stored in @pfn,
  * true indicates success, otherwise false is returned.  It's also the
  * only part that runs if we can in atomic context.
  */
@@ -2522,10 +2522,9 @@ static bool hva_to_pfn_fast(unsigned long addr, bool write_fault,
 	 * or the caller allows to map a writable pfn for a read fault
 	 * request.
 	 */
-	if (!(write_fault || writable))
-		return false;
+	unsigned int gup_flags = (write_fault || writable) ? FOLL_WRITE : 0;
 
-	if (get_user_page_fast_only(addr, FOLL_WRITE, page)) {
+	if (get_user_page_fast_only(addr, gup_flags, page)) {
 		*pfn = page_to_pfn(page[0]);
 
 		if (writable)
