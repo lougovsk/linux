9a8d28a91089bc477e17fecc2c38385830cf252d
Mark Brown <broonie@kernel.org>
2023-10-09 13:08:57 +0100

    arm64/mm: Implement map_shadow_stack()
    As discussed extensively in the changelog for the addition of this
    syscall on x86 ("x86/shstk: Introduce map_shadow_stack syscall") the
    existing mmap() and madvise() syscalls do not map entirely well onto the
    security requirements for guarded control stacks since they lead to
    windows where memory is allocated but not yet protected or stacks which
    are not properly and safely initialised. Instead a new syscall
    map_shadow_stack() has been defined which allocates and initialises a
    shadow stack page.
    
    Implement this for arm64.  Two flags are provided, allowing applications
    to request that the stack be initialised with a valid cap token at the
    top of the stack and optionally also an end of stack marker above that.
    We support requesting an end of stack marker alone but since this is a
    NULL pointer it is indistinguishable from not initialising anything by
    itself.
    
    Since the x86 code has not yet been rebased to v6.5-rc1 this includes
    the architecture neutral parts of Rick Edgecmbe's "x86/shstk: Introduce
    map_shadow_stack syscall".
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Message-Id: <20231009-arm64-gcs-v6-23-78e55deaa4dd@kernel.org>

--- arch/arm64/mm/gcs.c
+++ arch/arm64/mm/gcs.c
@@ -43,7 +43,6 @@ unsigned long gcs_alloc_thread_stack(struct task_struct *tsk,
 	unsigned long addr;
 
 	size = gcs_size(size);
-
 	addr = alloc_gcs(0, size, 0, 0);
 	if (IS_ERR_VALUE(addr))
 		return addr;
@@ -51,6 +50,66 @@ unsigned long gcs_alloc_thread_stack(struct task_struct *tsk,
 	tsk->thread.gcs_base = addr;
 	tsk->thread.gcs_size = size;
 	tsk->thread.gcspr_el0 = addr + size - sizeof(u64);
+
+	return addr;
+}
+
+SYSCALL_DEFINE3(map_shadow_stack, unsigned long, addr, unsigned long, size, unsigned int, flags)
+{
+	unsigned long alloc_size;
+	unsigned long __user *cap_ptr;
+	unsigned long cap_val;
+	int ret, cap_offset;
+
+	if (!system_supports_gcs())
+		return -EOPNOTSUPP;
+
+	if (flags & ~(SHADOW_STACK_SET_TOKEN | SHADOW_STACK_SET_MARKER))
+		return -EINVAL;
+
+	if (addr && (addr % PAGE_SIZE))
+		return -EINVAL;
+
+	if (size == 8 || size % 8)
+		return -EINVAL;
+
+	/*
+	 * An overflow would result in attempting to write the restore token
+	 * to the wrong location. Not catastrophic, but just return the right
+	 * error code and block it.
+	 */
+	alloc_size = PAGE_ALIGN(size);
+	if (alloc_size < size)
+		return -EOVERFLOW;
+
+	addr = alloc_gcs(addr, alloc_size, 0, false);
+	if (IS_ERR_VALUE(addr))
+		return addr;
+
+	/*
+	 * Put a cap token at the end of the allocated region so it
+	 * can be switched to.
+	 */
+	if (flags & SHADOW_STACK_SET_TOKEN) {
+		/* Leave an extra empty frame as a top of stack marker? */
+		if (flags & SHADOW_STACK_SET_MARKER)
+			cap_offset = 2;
+		else
+			cap_offset = 1;
+
+		cap_ptr = (unsigned long __user *)(addr + size -
+						   (cap_offset * sizeof(unsigned long)));
+		cap_val = GCS_CAP(cap_ptr);
+
+		ret = copy_to_user_gcs(cap_ptr, &cap_val, 1);
+		if (ret != 0) {
+			vm_munmap(addr, size);
+			return -EFAULT;
+		}
+
+		/* Ensure the new cap is viaible for GCS */
+		gcsb_dsync();
+	}
 
 	return addr;
 }
