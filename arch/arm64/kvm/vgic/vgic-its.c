c6087c856eef02de9e49667658e40e85cc51b6ba
Kunkun Jiang <jiangkunkun@huawei.com>
2024-06-20 21:06:49 +0800

    KVM: arm64: vgic-its: Clear dte when mapd unmaps a device
    vgic_its_save_device_tables will traverse its->device_list to
    save dte for each device. vgic_its_restore_device_tables will
    traverse each entry of device table and check if it is valid.
    Restore if valid.
    
    But when mapd unmaps a devices, we do not invalidate the
    corresponding dte. In the scenario of continuous save
    and restore, there may be a situation where a device's dte
    is not saved but is restored. This is unreasonable and may
    cause restore to fail. This patch clears the corresponding
    dte when mapd unmaps a device.
    
    Singed-off-by: Shusen Li <lishusen2@huawei.com>
    Co-developed-by: Shusen Li <lishusen2@huawei.com>
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Message-Id: <20240620130650.1279-3-jiangkunkun@huawei.com>

--- arch/arm64/kvm/vgic/vgic-its.c
+++ arch/arm64/kvm/vgic/vgic-its.c
@@ -1140,8 +1140,9 @@ static int vgic_its_cmd_handle_mapd(struct kvm *kvm, struct vgic_its *its,
 	u8 num_eventid_bits = its_cmd_get_size(its_cmd);
 	gpa_t itt_addr = its_cmd_get_ittaddr(its_cmd);
 	struct its_device *device;
+	gpa_t gpa;
 
-	if (!vgic_its_check_id(its, its->baser_device_table, device_id, NULL))
+	if (!vgic_its_check_id(its, its->baser_device_table, device_id, &gpa))
 		return E_ITS_MAPD_DEVICE_OOR;
 
 	if (valid && num_eventid_bits > VITS_TYPER_IDBITS)
@@ -1161,8 +1162,14 @@ static int vgic_its_cmd_handle_mapd(struct kvm *kvm, struct vgic_its *its,
 	 * The spec does not say whether unmapping a not-mapped device
 	 * is an error, so we are done in any case.
 	 */
-	if (!valid)
+	if (!valid) {
+		int dte_esz = vgic_its_get_abi(its)->dte_esz;
+		u64 val = 0;
+
+		BUG_ON(dte_esz > sizeof(val));
+		vgic_write_guest_lock(kvm, gpa, &val, dte_esz);
 		return 0;
+	}
 
 	device = vgic_its_alloc_device(its, device_id, itt_addr,
 				       num_eventid_bits);
