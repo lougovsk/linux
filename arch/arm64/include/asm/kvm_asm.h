6158ae9aaa1bc1a49e99c74bb1f05fb4f1d21cb7
Vincent Donnefort <vdonnefort@google.com>
2024-08-05 18:32:30 +0100

    KVM: arm64: Add tracing support for the pKVM hyp
    When running with protected mode, the host has very little knowledge
    about what is happening in the hypervisor. Of course this is an
    essential feature for security but nonetheless, that piece of code
    growing with more responsibilities, we need now a way to debug and
    profile it. Tracefs by its reliatility, versatility and support for
    user-space is the perfect tool.
    
    There's no way the hypervisor could log events directly into the host
    tracefs ring-buffers. So instead let's use our own, where the hypervisor
    is the writer and the host the reader.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>
    
    Message-Id: <20240805173234.3542917-8-vdonnefort@google.com>

--- arch/arm64/include/asm/kvm_asm.h
+++ arch/arm64/include/asm/kvm_asm.h
@@ -79,6 +79,10 @@ enum __kvm_host_smccc_func {
 	__KVM_HOST_SMCCC_FUNC___pkvm_init_vm,
 	__KVM_HOST_SMCCC_FUNC___pkvm_init_vcpu,
 	__KVM_HOST_SMCCC_FUNC___pkvm_teardown_vm,
+	__KVM_HOST_SMCCC_FUNC___pkvm_load_tracing,
+	__KVM_HOST_SMCCC_FUNC___pkvm_teardown_tracing,
+	__KVM_HOST_SMCCC_FUNC___pkvm_enable_tracing,
+	__KVM_HOST_SMCCC_FUNC___pkvm_swap_reader_tracing,
 };
 
 #define DECLARE_KVM_VHE_SYM(sym)	extern char sym[]
