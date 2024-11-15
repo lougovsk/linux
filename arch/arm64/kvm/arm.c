3648112c83369ccba3fb537c3ea5a1df72e1fb0d
Oliver Upton <oliver.upton@linux.dev>
2024-11-15 14:49:10 -0800

    KVM: arm64: Get rid of __kvm_get_mdcr_el2() and related warts
    KVM caches MDCR_EL2 on a per-CPU basis in order to preserve the
    configuration of MDCR_EL2.HPMN while running a guest. This is a bit
    gross, since we're relying on some baked configuration rather than the
    hardware definition of implemented counters.
    
    Discover the number of implemented counters by reading PMCR_EL0.N
    instead. This works because:
    
     - In VHE the kernel runs at EL2, and N always returns the number of
       counters implemented in hardware
    
     - In {n,h}VHE, the EL2 setup code programs MDCR_EL2.HPMN with the EL2
       view of PMCR_EL0.N for the host
    
    Lastly, avoid traps under nested virtualization by saving PMCR_EL0.N in
    host data.
    
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Message-Id: <20241115224924.2132364-3-oliver.upton@linux.dev>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -2129,6 +2129,7 @@ static void cpu_set_hyp_vector(void)
 static void cpu_hyp_init_context(void)
 {
 	kvm_init_host_cpu_context(host_data_ptr(host_ctxt));
+	kvm_init_host_debug_data();
 
 	if (!is_kernel_in_hyp_mode())
 		cpu_init_hyp_mode();
@@ -2137,7 +2138,6 @@ static void cpu_hyp_init_context(void)
 static void cpu_hyp_init_features(void)
 {
 	cpu_set_hyp_vector();
-	kvm_arm_init_debug();
 
 	if (is_kernel_in_hyp_mode())
 		kvm_timer_init_vhe();
