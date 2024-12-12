8135d426c5acde61d47a6cb6ac91bca0658095d5
Steven Price <steven.price@arm.com>
2024-12-12 15:55:33 +0000

    arm64: RME: ioctls to create and configure realms
    Add the KVM_CAP_ARM_RME_CREATE_RD ioctl to create a realm. This involves
    delegating pages to the RMM to hold the Realm Descriptor (RD) and for
    the base level of the Realm Translation Tables (RTT). A VMID also need
    to be picked, since the RMM has a separate VMID address space a
    dedicated allocator is added for this purpose.
    
    KVM_CAP_ARM_RME_CONFIG_REALM is provided to allow configuring the realm
    before it is created. Configuration options can be classified as:
    
     1. Parameters specific to the Realm stage2 (e.g. IPA Size, vmid, stage2
        entry level, entry level RTTs, number of RTTs in start level, LPA2)
        Most of these are not measured by RMM and comes from KVM book
        keeping.
    
     2. Parameters controlling "Arm Architecture features for the VM". (e.g.
        SVE VL, PMU counters, number of HW BRPs/WPs), configured by the VMM
        using the "user ID register write" mechanism. These will be
        supported in the later patches.
    
     3. Parameters are not part of the core Arm architecture but defined
        by the RMM spec (e.g. Hash algorithm for measurement,
        Personalisation value). These are programmed via
        KVM_CAP_ARM_RME_CONFIG_REALM.
    
    For the IPA size there is the possibility that the RMM supports a
    different size to the IPA size supported by KVM for normal guests. At
    the moment the 'normal limit' is exposed by KVM_CAP_ARM_VM_IPA_SIZE and
    the IPA size is configured by the bottom bits of vm_type in
    KVM_CREATE_VM. This means that it isn't easy for the VMM to discover
    what IPA sizes are supported for Realm guests. Since the IPA is part of
    the measurement of the realm guest the current expectation is that the
    VMM will be required to pick the IPA size demanded by attestation and
    therefore simply failing if this isn't available is fine. An option
    would be to expose a new capability ioctl to obtain the RMM's maximum
    IPA size if this is needed in the future.
    
    Co-developed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    Message-Id: <20241212155610.76522-9-steven.price@arm.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -863,11 +863,15 @@ static struct kvm_pgtable_mm_ops kvm_s2_mm_ops = {
 	.icache_inval_pou	= invalidate_icache_guest_page,
 };
 
-static int kvm_init_ipa_range(struct kvm_s2_mmu *mmu, unsigned long type)
+static int kvm_init_ipa_range(struct kvm *kvm,
+			      struct kvm_s2_mmu *mmu, unsigned long type)
 {
 	u32 kvm_ipa_limit = get_kvm_ipa_limit();
 	u64 mmfr0, mmfr1;
 	u32 phys_shift;
+
+	if (kvm_is_realm(kvm))
+		kvm_ipa_limit = kvm_realm_ipa_limit();
 
 	if (type & ~KVM_VM_TYPE_ARM_IPA_SIZE_MASK)
 		return -EINVAL;
@@ -933,7 +937,7 @@ int kvm_init_stage2_mmu(struct kvm *kvm, struct kvm_s2_mmu *mmu, unsigned long t
 		return -EINVAL;
 	}
 
-	err = kvm_init_ipa_range(mmu, type);
+	err = kvm_init_ipa_range(kvm, mmu, type);
 	if (err)
 		return err;
 
@@ -1056,6 +1060,20 @@ void kvm_free_stage2_pgd(struct kvm_s2_mmu *mmu)
 	struct kvm_pgtable *pgt = NULL;
 
 	write_lock(&kvm->mmu_lock);
+	if (kvm_is_realm(kvm) &&
+	    (kvm_realm_state(kvm) != REALM_STATE_DEAD &&
+	     kvm_realm_state(kvm) != REALM_STATE_NONE)) {
+		/* Tearing down RTTs will be added in a later patch */
+		write_unlock(&kvm->mmu_lock);
+
+		/*
+		 * The physical PGD pages are delegated to the RMM, so cannot
+		 * be freed at this point. This function will be called again
+		 * from kvm_destroy_realm() after the physical pages have been
+		 * returned at which point the memory can be freed.
+		 */
+		return;
+	}
 	pgt = mmu->pgt;
 	if (pgt) {
 		mmu->pgd_phys = 0;
