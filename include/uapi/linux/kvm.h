e234f3aa1e2c85f0025bd1ee6767aa89a2eaaabf
Marc Zyngier <maz@kernel.org>
2025-02-15 17:38:15 +0000

    KVM: arm64: Allow userspace to request KVM_ARM_VCPU_EL2*
    Since we're (almost) feature complete, let's allow userspace to
    request KVM_ARM_VCPU_EL2* by bumping KVM_VCPU_MAX_FEATURES up.
    
    We also now advertise the features to userspace with a new capabilities.
    
    It's going to be great...
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Message-Id: <20250215173816.3767330-14-maz@kernel.org>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -929,6 +929,8 @@ struct kvm_enable_cap {
 #define KVM_CAP_PRE_FAULT_MEMORY 236
 #define KVM_CAP_X86_APIC_BUS_CYCLES_NS 237
 #define KVM_CAP_X86_GUEST_MODE 238
+#define KVM_CAP_ARM_EL2 239
+#define KVM_CAP_ARM_EL2_E2H0 240
 
 struct kvm_irq_routing_irqchip {
 	__u32 irqchip;
