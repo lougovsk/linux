37abf0049940f9aa5ccf2794170be822b0ecd405
Vincent Donnefort <vdonnefort@google.com>
2025-05-16 14:40:29 +0100

    KVM: arm64: Add hyp_enter/hyp_exit events to pKVM hyp
    The hyp_enter and hyp_exit events are logged by the hypervisor any time
    it is entered and exited.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>
    
    Message-Id: <20250516134031.661124-24-vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/psci-relay.c
+++ arch/arm64/kvm/hyp/nvhe/psci-relay.c
@@ -6,11 +6,12 @@
 
 #include <asm/kvm_asm.h>
 #include <asm/kvm_hyp.h>
+#include <asm/kvm_hypevents.h>
 #include <asm/kvm_mmu.h>
-#include <linux/arm-smccc.h>
 #include <linux/kvm_host.h>
 #include <uapi/linux/psci.h>
 
+#include <nvhe/arm-smccc.h>
 #include <nvhe/memory.h>
 #include <nvhe/trap_handler.h>
 
@@ -205,6 +206,7 @@ asmlinkage void __noreturn __kvm_host_psci_cpu_entry(bool is_cpu_on)
 	struct psci_boot_args *boot_args;
 	struct kvm_cpu_context *host_ctxt;
 
+	trace_hyp_enter();
 	host_ctxt = host_data_ptr(host_ctxt);
 
 	if (is_cpu_on)
@@ -221,6 +223,7 @@ asmlinkage void __noreturn __kvm_host_psci_cpu_entry(bool is_cpu_on)
 	write_sysreg_el1(INIT_SCTLR_EL1_MMU_OFF, SYS_SCTLR);
 	write_sysreg(INIT_PSTATE_EL1, SPSR_EL2);
 
+	trace_hyp_exit();
 	__host_enter(host_ctxt);
 }
 
