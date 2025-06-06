01a35fe7d99357fbfb368fb204cdfcc6ef311117
Vipin Sharma <vipinsh@google.com>
2025-06-06 16:56:09 -0700

    KVM: selftests: Run tests concurrently in KVM selftests runner
    Add a command line argument, --jobs, to specify how many tests can
    execute concurrently. Set default to 1.
    
    Example:
      python3 runner --test-dirs tests -j 10
    
    Signed-off-by: Vipin Sharma <vipinsh@google.com>
    Message-Id: <20250606235619.1841595-6-vipinsh@google.com>

--- tools/testing/selftests/kvm/runner/test_runner.py
+++ tools/testing/selftests/kvm/runner/test_runner.py
@@ -4,6 +4,8 @@
 # Author: vipinsh@google.com (Vipin Sharma)
 
 import logging
+import concurrent.futures
+
 from selftest import Selftest
 from selftest import SelftestStatus
 
@@ -14,10 +16,15 @@ class TestRunner:
     def __init__(self, test_files, args):
         self.tests = []
         self.output_dir = args.output
+        self.jobs = args.jobs
 
         for test_file in test_files:
             self.tests.append(Selftest(test_file, args.executable,
                                        args.timeout, args.output))
+
+    def _run_test(self, test):
+        test.run()
+        return test
 
     def _log_result(self, test_result):
         logger.log(test_result.status,
@@ -33,12 +40,17 @@ class TestRunner:
     def start(self):
         ret = 0
 
-        for test in self.tests:
-            test.run()
-            self._log_result(test)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=self.jobs) as executor:
+            all_futures = []
+            for test in self.tests:
+                future = executor.submit(self._run_test, test)
+                all_futures.append(future)
 
-            if (test.status not in [SelftestStatus.PASSED,
-                                    SelftestStatus.NO_RUN,
-                                    SelftestStatus.SKIPPED]):
-                ret = 1
+            for future in concurrent.futures.as_completed(all_futures):
+                test_result = future.result()
+                self._log_result(test_result)
+                if (test_result.status not in [SelftestStatus.PASSED,
+                                               SelftestStatus.NO_RUN,
+                                               SelftestStatus.SKIPPED]):
+                    ret = 1
         return ret
