cd866f47f37fddd479ee0320d3fa533388b89ae5
Steven Price <steven.price@arm.com>
2025-06-11 11:48:27 +0100

    arm64: RME: Prevent Device mappings for Realms
    Physical device assignment is not yet supported by the RMM, so it
    doesn't make much sense to allow device mappings within the realm.
    Prevent them when the guest is a realm.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Message-Id: <20250611104844.245235-31-steven.price@arm.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -1188,6 +1188,10 @@ int kvm_phys_addr_ioremap(struct kvm *kvm, phys_addr_t guest_ipa,
 	if (is_protected_kvm_enabled())
 		return -EPERM;
 
+	/* We don't support mapping special pages into a Realm */
+	if (kvm_is_realm(kvm))
+		return -EPERM;
+
 	size += offset_in_page(guest_ipa);
 	guest_ipa &= PAGE_MASK;
 
@@ -1787,6 +1791,15 @@ static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 
 	if (exec_fault && device)
 		return -ENOEXEC;
+
+	/*
+	 * For now we shouldn't be hitting protected addresses because they are
+	 * handled in private_memslot_fault(). In the future this check may be
+	 * relaxed to support e.g. protected devices.
+	 */
+	if (vcpu_is_rec(vcpu) &&
+	    kvm_gpa_from_fault(kvm, fault_ipa) == fault_ipa)
+		return -EINVAL;
 
 	/*
 	 * Potentially reduce shadow S2 permissions to match the guest's own
