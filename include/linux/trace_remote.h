eed126bfc52e226ae4b36ed9a57f2af7dfcc56cc
Vincent Donnefort <vdonnefort@google.com>
2025-08-21 09:13:51 +0100

    tracing: Introduce trace remotes
    A trace remote relies on ring-buffer remotes to read and control
    compatible tracing buffers, written by entity such as firmware or
    hypervisor.
    
    Add a Tracefs directory remotes/ that contains all instances of trace
    remotes. Each instance follows the same hierarchy as any other to ease
    the support by existing user-space tools.
    
    This currently does not provide any event support, which will come
    later.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>
    
    Message-Id: <20250821081412.1008261-4-vdonnefort@google.com>

---
+++ include/linux/trace_remote.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _LINUX_TRACE_REMOTE_H
+#define _LINUX_TRACE_REMOTE_H
+
+#include <linux/ring_buffer.h>
+
+struct trace_remote_callbacks {
+	struct trace_buffer_desc *
+		(*load_trace_buffer)(unsigned long size, void *priv);
+	void	(*unload_trace_buffer)(struct trace_buffer_desc *desc, void *priv);
+	int	(*enable_tracing)(bool enable, void *priv);
+	int	(*swap_reader_page)(unsigned int cpu, void *priv);
+};
+
+int trace_remote_register(const char *name, struct trace_remote_callbacks *cbs, void *priv);
+int trace_remote_alloc_buffer(struct trace_buffer_desc *desc, size_t size,
+			      const struct cpumask *cpumask);
+void trace_remote_free_buffer(struct trace_buffer_desc *desc);
+
+#endif
