75406a7d70ddee772f831d48cfaa02af041f1594
Fuad Tabba <tabba@google.com>
2025-11-04 12:59:01 +0000

    KVM: arm64: Enable LS64 instructions when supported by guest
    The FEAT_LS64* family (FEAT_LS64, FEAT_LS64_V, FEAT_LS64_ACCDATA)
    enables support for LD64B, ST64B, and their variants. If a guest is
    advertised these features, KVM should not trap accesses to these
    instructions to EL2.
    
    This is controlled by the HCRX_EL2_EnASR, HCRX_EL2_EnALS, and
    HCRX_EL2_EnAS0 bits. When clear, these bits trap the corresponding
    instructions. KVM did not set them, which would cause guest-supported
    instructions to trap.
    
    This also created a state mismatch for nested virtualization, which
    validates its own HCRX_EL2 value against the features advertised in the
    guest's ID_AA64ISAR1_EL1 (in handle_other()).
    
    Fix this by checking for each FEAT_LS64* variant in
    vcpu_set_hcrx() and setting the corresponding HCRX_EL2 enable
    bit if the guest supports the feature.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Message-Id: <20251104125906.1919426-4-tabba@google.com>

--- arch/arm64/include/asm/kvm_emulate.h
+++ arch/arm64/include/asm/kvm_emulate.h
@@ -691,6 +691,15 @@ static inline void vcpu_set_hcrx(struct kvm_vcpu *vcpu)
 		if (!kvm_has_feat(kvm, ID_AA64PFR1_EL1, NMI, IMP))
 			vcpu->arch.hcrx_el2 |= HCRX_EL2_TALLINT;
 
+		if (kvm_has_feat(kvm, ID_AA64ISAR1_EL1, LS64, LS64_V))
+			vcpu->arch.hcrx_el2 |= HCRX_EL2_EnASR;
+
+		if (kvm_has_feat(kvm, ID_AA64ISAR1_EL1, LS64, LS64))
+			vcpu->arch.hcrx_el2 |= HCRX_EL2_EnALS;
+
+		if (kvm_has_feat(kvm, ID_AA64ISAR1_EL1, LS64, LS64_ACCDATA))
+			vcpu->arch.hcrx_el2 |= HCRX_EL2_EnAS0;
+
 		if (kvm_has_tcr2(kvm))
 			vcpu->arch.hcrx_el2 |= HCRX_EL2_TCR2En;
 
