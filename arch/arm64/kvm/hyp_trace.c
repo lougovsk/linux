1ab7d101c30b3635db59e8d0550254260452ee7c
Vincent Donnefort <vdonnefort@google.com>
2025-11-07 09:38:36 +0000

    KVM: arm64: Add trace reset to the pKVM hyp
    Let the hypervisor reset the trace buffer when triggered from the
    tracefs file remotes/hypervisor/trace.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>
    
    Message-Id: <20251107093840.3779150-25-vdonnefort@google.com>

--- arch/arm64/kvm/hyp_trace.c
+++ arch/arm64/kvm/hyp_trace.c
@@ -302,7 +302,7 @@ static int hyp_trace_swap_reader_page(unsigned int cpu, void *priv)
 
 static int hyp_trace_reset(unsigned int cpu, void *priv)
 {
-	return 0;
+	return kvm_call_hyp_nvhe(__pkvm_reset_tracing, cpu);
 }
 
 static int hyp_trace_enable_event(unsigned short id, bool enable, void *priv)
