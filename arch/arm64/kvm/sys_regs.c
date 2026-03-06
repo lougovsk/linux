90c9690f04bf56d1893312735593659c3d6c2c24
Mark Brown <broonie@kernel.org>
2026-03-06 17:01:10 +0000

    KVM: arm64: Support SME priority registers
    SME has optional support for configuring the relative priorities of PEs
    in systems where they share a single SME hardware block, known as a
    SMCU. Currently we do not have any support for this in Linux and will
    also hide it from KVM guests, pending experience with practical
    implementations. The interface for configuring priority support is via
    two new system registers, these registers are always defined when SME is
    available.
    
    The register SMPRI_EL1 allows control of SME execution priorities. Since
    we disable SME priority support for guests this register is RES0, define
    it as such and enable fine grained traps for SMPRI_EL1 to ensure that
    guests can't write to it even if the hardware supports priorities.  Since
    the register should be readable with fixed contents we only trap writes,
    not reads. Since there is no host support for using priorities the
    register currently left with a value of 0 by the host so we do not need
    to update the value for guests.
    
    There is also an EL2 register SMPRIMAP_EL2 for virtualisation of
    priorities, this is RES0 when priority configuration is not supported
    but has no specific traps available.  When saving state from a nested
    guest we overwrite any value the guest stored.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Message-Id: <20260306-kvm-arm64-sme-v10-18-43f7683a0fb7@kernel.org>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -691,6 +691,15 @@ static bool trap_raz_wi(struct kvm_vcpu *vcpu,
 		return read_zero(vcpu, p);
 }
 
+static int set_res0(struct kvm_vcpu *vcpu, const struct sys_reg_desc *rd,
+		    u64 val)
+{
+	if (val)
+		return -EINVAL;
+
+	return 0;
+}
+
 /*
  * ARMv8.1 mandates at least a trivial LORegion implementation, where all the
  * RW registers are RES0 (which we can implement as RAZ/WI). On an ARMv8.0
@@ -1975,6 +1984,15 @@ static unsigned int fp8_visibility(const struct kvm_vcpu *vcpu,
 {
 	if (kvm_has_fpmr(vcpu->kvm))
 		return 0;
+
+	return REG_HIDDEN;
+}
+
+static unsigned int sme_raz_visibility(const struct kvm_vcpu *vcpu,
+				       const struct sys_reg_desc *rd)
+{
+	if (vcpu_has_sme(vcpu))
+		return REG_RAZ;
 
 	return REG_HIDDEN;
 }
@@ -3371,7 +3389,14 @@ static const struct sys_reg_desc sys_reg_descs[] = {
 
 	{ SYS_DESC(SYS_ZCR_EL1), NULL, reset_val, ZCR_EL1, 0, .visibility = sve_visibility },
 	{ SYS_DESC(SYS_TRFCR_EL1), undef_access },
-	{ SYS_DESC(SYS_SMPRI_EL1), undef_access },
+
+	/*
+	 * SMPRI_EL1 is UNDEF when SME is disabled, the UNDEF is
+	 * handled via FGU which is handled without consulting this
+	 * table.
+	 */
+	{ SYS_DESC(SYS_SMPRI_EL1), trap_raz_wi, .visibility = sme_raz_visibility },
+
 	{ SYS_DESC(SYS_SMCR_EL1), NULL, reset_val, SMCR_EL1, 0, .visibility = sme_visibility },
 	{ SYS_DESC(SYS_TTBR0_EL1), access_vm_reg, reset_unknown, TTBR0_EL1 },
 	{ SYS_DESC(SYS_TTBR1_EL1), access_vm_reg, reset_unknown, TTBR1_EL1 },
@@ -3742,6 +3767,9 @@ static const struct sys_reg_desc sys_reg_descs[] = {
 
 	EL2_REG_VNCR(HCRX_EL2, reset_val, 0),
 
+	{ SYS_DESC(SYS_SMPRIMAP_EL2), .reg = SMPRIMAP_EL2,
+	  .access = trap_raz_wi, .set_user = set_res0, .reset = reset_val,
+	  .val = 0, .visibility = sme_el2_visibility },
 	EL2_REG_FILTERED(SMCR_EL2, access_smcr_el2, reset_val, 0,
 			 sme_el2_visibility),
 
