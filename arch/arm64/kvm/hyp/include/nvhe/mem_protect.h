b6f52b34eaaa14bc6ba8cc844b4ce92d5db92e14
Mostafa Saleh <smostafa@google.com>
2026-03-10 12:49:20 +0000

    KVM: arm64: Donate MMIO to the hypervisor
    Add a function to donate MMIO to the hypervisor so IOMMU hypervisor
    drivers can use that to protect the MMIO of IOMMU.
    The initial attempt to implement this was to have a new flag to
    "___pkvm_host_donate_hyp" to accept MMIO. However that had many problems,
    it was quite intrusive for host/hyp to check/set page state to make it
    aware of MMIO and to encode the state in the page table in that case.
    Which is called in paths that can be sensitive to performance (FFA, VMs..)
    
    As donating MMIO is very rare, and we don’t need to encode the full
    state, it’s reasonable to have a separate function to do this.
    It will init the host s2 page table with an invalid leaf with the owner ID
    to prevent the host from mapping the page on faults.
    
    Also, prevent kvm_pgtable_stage2_unmap() from removing owner ID from
    stage-2 PTEs, as this can be triggered from recycle logic under memory
    pressure. There is no code relying on this, as all ownership changes is
    done via kvm_pgtable_stage2_set_owner()
    
    For error path in IOMMU drivers, add a function to donate MMIO back
    from hyp to host.
    
    Signed-off-by: Mostafa Saleh <smostafa@google.com>
    Reviewed-by: Fuad Tabba <tabba@google.com>
    Message-Id: <20260310124933.830025-2-sebastianene@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -31,6 +31,8 @@ enum pkvm_component_id {
 };
 
 extern unsigned long hyp_nr_cpus;
+int __pkvm_host_donate_hyp_mmio(u64 pfn);
+int __pkvm_hyp_donate_host_mmio(u64 pfn);
 
 int __pkvm_prot_finalize(void);
 int __pkvm_host_share_hyp(u64 pfn);
