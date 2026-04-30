57f86a53ba2ca7eb2b0c3bb242435f91daf25d69
Leonardo Bras <leo.bras@arm.com>
2026-04-30 12:14:11 +0100

    kvm: Add arch-generic interface for hw-accelerated dirty-bitmap cleaning
    Introduce kvm_arch_dirty_log_clear() that allow implementation of
    arch-specific hardware-accelerated dirty-log routines.
    
    A call to that is added on both kvm_get_dirty_log_protect() and
    kvm_clear_dirty_log_protect() and will fall back to software version if
    not implemented, or any error was detected in the arch-specific routine.
    
    For an arch to implement this function, it's required to provide an
    asm/kvm_dirty_bit.h and have CONFIG_HAVE_KVM_HW_DIRTY_BIT=y on building.
    
    If the arch does not implement it, and thus lack above config, the
    introduced snippet is expected to be compiled-out and have zero impact at
    runtime.
    
    Signed-off-by: Leonardo Bras <leo.bras@arm.com>
    Message-Id: <20260430111424.3479613-9-leo.bras@arm.com>

---
+++ include/linux/kvm_dirty_bit.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2026 ARM Ltd.
+ * Author: Leonardo Bras <leo.bras@arm.com>
+ */
+
+#ifndef __KVM_DIRTY_BIT_H__
+#define __KVM_DIRTY_BIT_H__
+
+#ifndef CONFIG_HAVE_KVM_HW_DIRTY_BIT
+
+static inline int kvm_arch_dirty_log_clear(struct kvm *kvm,
+					   struct kvm_memory_slot *memslot,
+					   struct kvm_clear_dirty_log *log,
+					   unsigned long *bitmap,
+					   bool *flush)
+{
+	return -ENXIO;
+}
+
+#else /* CONFIG_HAVE_KVM_HW_DIRTY_BIT */
+
+#include <asm/kvm_dirty_bit.h>
+
+#endif /* CONFIG_HAVE_KVM_HW_DIRTY_BIT */
+
+#endif /* __KVM_DIRTY_BIT_H__ */
