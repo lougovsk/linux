79f78dd92dbf86d34556b75b2858d55b24d4f3bd
Leonardo Bras <leo.bras@arm.com>
2026-04-30 12:14:10 +0100

    KVM: arm64: dirty_bit: Add base FEAT_HACDBS cleaning routine
    Introduce the basic cleaning routine that is going to be used for both
    dirty-bitmap and dirty-ring routines.
    
    It sets the required registers with the input buffer, and wait for
    HACDBSIRQ to happen, which means either the task is done, or there was some
    error during processing.
    
    It is ran with preemption disabled, as a task being scheduled in could
    change the translation registers used by HACDBS and end up corrupting the
    current dirty-bit tracking and the sched-in task's S2 pagetables.
    
    Signed-off-by: Leonardo Bras <leo.bras@arm.com>
    Message-Id: <20260430111424.3479613-8-leo.bras@arm.com>

--- arch/arm64/kvm/dirty_bit.c
+++ arch/arm64/kvm/dirty_bit.c
@@ -5,6 +5,7 @@
  */
 
 #include <asm/kvm_dirty_bit.h>
+#include <asm/kvm_mmu.h>
 #include <linux/kconfig.h>
 #include <linux/acpi.h>
 
@@ -30,6 +31,91 @@ inline u64 hdbss_get_ttwl(u64 chunk_size)
 }
 
 static __ro_after_init int hacdbsirq = -1;
+
+static void hacdbs_start(u64 *hw_entries, int size)
+{
+	u64 br;
+	/* Each entry is 8 bytes */
+	int size_b = size * sizeof(hw_entries[0]);
+	int size_p2 = max(roundup_pow_of_two(size_b), PAGE_SIZE);
+
+	/* If not using the full size of the array, put a stop entry at the end */
+	if (size_b < size_p2)
+		hw_entries[size] = HDBSS_ENTRY_VALID | HDBSS_ENTRY_TTWL_RESV;
+
+	sysreg_clear_set_s(SYS_HACDBSCONS_EL2,
+			   HACDBSCONS_EL2_ERR_REASON | HACDBSCONS_EL2_INDEX, 0);
+
+	br = (virt_to_phys(hw_entries) & HACDBSBR_EL2_BADDR_MASK) |
+	     FIELD_PREP(HACDBSBR_EL2_SZ, ilog2(size_p2) - 12) |
+	     FIELD_PREP(HACDBSBR_EL2_EN, 1);
+
+	this_cpu_write(hacdbs_pcp.status, HACDBS_RUNNING);
+	this_cpu_write(hacdbs_pcp.size, size);
+	write_sysreg_s(br, SYS_HACDBSBR_EL2);
+	isb();
+}
+
+static int hacdbs_stop(void)
+{
+	write_sysreg_s(0, SYS_HACDBSBR_EL2);
+	isb();
+
+	if (this_cpu_read(hacdbs_pcp.status) == HACDBS_ERROR) {
+		/* In case of error, HACDBSCONS_EL2.INDEX should point the faulty entry */
+		u64 cons = read_sysreg_s(SYS_HACDBSCONS_EL2);
+		int idx = FIELD_GET(HACDBSCONS_EL2_INDEX, cons);
+
+		trace_printk("HACDBS found error %lu in index %d / %d\n",
+			     FIELD_GET(HACDBSCONS_EL2_ERR_REASON, cons), idx,
+			     this_cpu_read(hacdbs_pcp.size));
+
+		this_cpu_write(hacdbs_pcp.status, HACDBS_IDLE);
+
+		return idx;
+	}
+
+	return this_cpu_read(hacdbs_pcp.size);
+}
+
+/*
+ * Clears dirty-bits for an array of pages (hw_entries) using HACDBS
+ * Returns the number of items cleaned from the array. If returns value < size,
+ *	there was an error in the processing.
+ */
+static int dirty_bit_clear(struct kvm *kvm, u64 *hw_entries, int size)
+{
+	enum hacdbs_status st;
+	u64 hcr_el2;
+	int ret;
+
+	preempt_disable();
+
+	hcr_el2 = read_sysreg(HCR_EL2);
+	write_sysreg(hcr_el2 | HCR_EL2_VM, HCR_EL2);
+	__load_stage2(&kvm->arch.mmu, kvm->arch.mmu.arch);
+
+	hacdbs_start(hw_entries, size);
+
+	do {
+		wfi();
+	} while ((st = this_cpu_read(hacdbs_pcp.status)) == HACDBS_RUNNING);
+
+	ret = hacdbs_stop();
+
+	write_sysreg(hcr_el2, HCR_EL2);
+	isb();
+
+	/*
+	 * No DSB is needed here, as kvm_flush_remote_tlbs_memslot() that happens
+	 * later in generic dirty-cleaning code already performs a DSB before
+	 * doing the TLBI.
+	 */
+
+	preempt_enable();
+
+	return ret;
+}
 
 static irqreturn_t hacdbsirq_handler(int irq, void *pcpu)
 {
