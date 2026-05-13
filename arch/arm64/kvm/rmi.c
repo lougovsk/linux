97520289846eeb582256c1cf8992dc5a191783c8
Steven Price <steven.price@arm.com>
2026-05-13 14:17:20 +0100

    arm64: RMI: Check for LPA2 support
    If KVM has enabled LPA2 support then check that the RMM also supports
    it. If there is a mismatch then disable support for realm guests as the
    VMM may attempt to create a guest which is incompatible with the RMM.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Message-Id: <20260513131757.116630-13-steven.price@arm.com>

--- arch/arm64/kvm/rmi.c
+++ arch/arm64/kvm/rmi.c
@@ -5,8 +5,24 @@
 
 #include <linux/kvm_host.h>
 
+#include <asm/kvm_pgtable.h>
 #include <asm/rmi_cmds.h>
 #include <asm/virt.h>
+
+static bool rmi_has_feature(unsigned long feature)
+{
+	return !!u64_get_bits(rmm_feat_reg0, feature);
+}
+
+static int rmm_check_features(void)
+{
+	if (kvm_lpa2_is_enabled() && !rmi_has_feature(RMI_FEATURE_REGISTER_0_LPA2)) {
+		kvm_err("RMM doesn't support LPA2");
+		return -ENXIO;
+	}
+
+	return 0;
+}
 
 void kvm_init_rmi(void)
 {
@@ -18,6 +34,9 @@ void kvm_init_rmi(void)
 		return;
 
 	if (!rmi_is_available())
+		return;
+
+	if (rmm_check_features())
 		return;
 
 	/* Future patch will enable static branch kvm_rmi_is_available */
