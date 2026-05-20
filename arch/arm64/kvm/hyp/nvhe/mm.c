fa3ce02eee0c34d6a4a3f53f658ba911fa87f147
Vincent Donnefort <vdonnefort@google.com>
2026-05-20 16:26:34 +0100

    KVM: arm64: Add __pkvm_private_range_pa
    Mappings in the pKVM private range are not identity mapped, making the
    standard __hyp_pa() unsuitable for translating these addresses.
    
    Introduce __pkvm_private_range_pa() to resolve physical addresses for
    this range by walking the hypervisor page-table. This will be useful for
    the upcoming pKVM heap allocator.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>
    
    Message-Id: <20260520152650.4107895-2-vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/mm.c
+++ arch/arm64/kvm/hyp/nvhe/mm.c
@@ -502,3 +502,17 @@ int refill_memcache(struct kvm_hyp_memcache *mc, unsigned long min_pages,
 
 	return ret;
 }
+
+phys_addr_t __pkvm_private_range_pa(void *va)
+{
+	kvm_pte_t pte = 0;
+	s8 level;
+
+	hyp_spin_lock(&pkvm_pgd_lock);
+	WARN_ON(kvm_pgtable_get_leaf(&pkvm_pgtable, (u64)va, &pte, &level));
+	hyp_spin_unlock(&pkvm_pgd_lock);
+
+	WARN_ON(!kvm_pte_valid(pte));
+
+	return kvm_pte_to_phys(pte) + offset_in_page(va);
+}
