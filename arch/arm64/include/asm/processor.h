8f400dff5828934b73875763b71a49bac455189c
Mark Rutland <mark.rutland@arm.com>
2026-05-28 17:54:42 +0100

    arm64: fpsimd: Use opaque type for SME state
    As the SME state size can vary at runtime, we don't have a concrete type
    for the in-memory SME state, and pass this around using a pointer to
    void.
    
    Using pointer to void means that it's very easy to introduce errors that
    cannot be caught by the compiler (e.g. as 'void **' can be assigned to
    'void *').
    
    Improve this by adding an opaque 'struct sve_state', and consistently
    passing a pointer to this.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Fuad Tabba <tabba@google.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Oliver Upton <oupton@kernel.org>
    Cc: Will Deacon <will@kernel.org>
    Message-Id: <20260528165446.701944-15-mark.rutland@arm.com>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -131,6 +131,7 @@ enum fp_type {
 };
 
 struct sve_state;		/* Opaque type */
+struct sme_state;		/* Opaque type */
 
 struct cpu_context {
 	unsigned long x19;
@@ -167,7 +168,7 @@ struct thread_struct {
 	enum fp_type		fp_type;	/* registers FPSIMD or SVE? */
 	unsigned int		fpsimd_cpu;
 	struct sve_state	*sve_state;	/* SVE registers, if any */
-	void			*sme_state;	/* ZA and ZT state, if any */
+	struct sme_state	*sme_state;	/* ZA and ZT state, if any */
 	unsigned int		vl[ARM64_VEC_MAX];	/* vector length */
 	unsigned int		vl_onexec[ARM64_VEC_MAX]; /* vl after next exec */
 	unsigned long		fault_address;	/* fault info */
