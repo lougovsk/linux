f785e93d74dec5acc9a0b126d9f97b555db77cc6
Mark Brown <broonie@kernel.org>
2026-07-09 19:27:26 +0100

    arm64/fpsimd: Determine maximum virtualisable SME vector length
    As with SVE we can only virtualise SME vector lengths that are supported by
    all CPUs in the system, implement similar checks to those for SVE. Since
    unlike SVE there are no specific vector lengths that are architecturally
    required the handling is subtly different, we report a system where this
    happens with a maximum vector length of 0.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Message-Id: <20260709-kvm-arm64-sme-v12-5-d0301d79ef58@kernel.org>

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -1277,6 +1277,7 @@ void __init sme_setup(void)
 	 * minimum available VL will be used.
 	 */
 	set_sme_default_vl(find_supported_vector_length(ARM64_VEC_SME, 32));
+	info->max_virtualisable_vl = vec_virtualisable_vl(info);
 
 	pr_info("SME: minimum available vector length %u bytes per vector\n",
 		info->min_vl);
@@ -1284,6 +1285,10 @@ void __init sme_setup(void)
 		info->max_vl);
 	pr_info("SME: default vector length %u bytes per vector\n",
 		get_sme_default_vl());
+
+	/* KVM decides whether to support mismatched systems. Just warn here: */
+	if (info->max_virtualisable_vl < info->max_vl)
+		pr_warn("SME: unvirtualisable vector lengths present\n");
 }
 
 void sme_suspend_exit(void)
