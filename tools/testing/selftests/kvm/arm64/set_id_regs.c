c2bc7ace4eb98f21dc09c0555f620fcb861f8631
Mark Brown <broonie@kernel.org>
2026-07-09 19:27:47 +0100

    KVM: arm64: selftests: Remove spurious check for single bit safe values
    get_safe_value() currently asserts that bitfields it is generating a safe
    value for must be more than one bit wide but in actual fact it should
    always be possible to generate a safe value to write to a bitfield even if
    it is just the current value and the function correctly handles that.
    Remove the assert.
    
    Fixes: bf09ee918053e ("KVM: arm64: selftests: Remove ARM64_FEATURE_FIELD_BITS and its last user")
    Reviewed-by: Ben Horgan <ben.horgan@arm.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Message-Id: <20260709-kvm-arm64-sme-v12-26-d0301d79ef58@kernel.org>

--- tools/testing/selftests/kvm/arm64/set_id_regs.c
+++ tools/testing/selftests/kvm/arm64/set_id_regs.c
@@ -279,8 +279,6 @@ u64 get_safe_value(const struct reg_ftr_bits *ftr_bits, u64 ftr)
 {
 	u64 ftr_max = ftr_bits->mask >> ftr_bits->shift;
 
-	TEST_ASSERT(ftr_max > 1, "This test doesn't support single bit features");
-
 	if (ftr_bits->sign == FTR_UNSIGNED) {
 		switch (ftr_bits->type) {
 		case FTR_EXACT:
